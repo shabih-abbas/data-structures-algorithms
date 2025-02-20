0208250ff5342c80ed65ed169f170792b737e721
Shabih Abbas <shabihabbas09@gmail.com>
2025-02-20 11:02:30 +0000

    problem 3.6 solved

--- greedy-algorithms/largest_number/largest_number.py
+++ greedy-algorithms/largest_number/largest_number.py
@@ -10,9 +10,13 @@ def largest_number_naive(numbers):
         largest = max(largest, int("".join(permutation)))
 
     return largest
+def largest_number_fast(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key= lambda x : int(x)/10**(len(x)-1),reverse=True)
+    return int("".join(numbers))
 
 
 if __name__ == '__main__':
     _ = int(input())
     input_numbers = input().split()
-    print(largest_number_naive(input_numbers))
+    print(largest_number_fast(input_numbers))
---
+++ greedy-algorithms/max_prizes/different_summands.py
@@ -0,0 +1,26 @@
+
+def optimal_summands(n):
+    summands = []
+    rest = n
+    i = 1
+    while not rest - i <= i:
+        summands.append(i)
+        rest-=i
+        i += 1
+    # for i in range(1,n//2+1):
+    #     if rest == i: 
+    #         summands.append(i)
+    #         return summands
+    #     if rest - i > i:
+    #         summands.append(i)
+    #         rest -= i
+    #     else : break
+    summands.append(rest)
+    return summands
+
+
+if __name__ == '__main__':
+    n = int(input())
+    summands = optimal_summands(n)
+    print(len(summands))
+    print(*summands)
